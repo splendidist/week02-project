decd4ae0b196231d5cf04c380154fbdd598642af
Lucy Holmes <lucyholmes08@hotmail.com>
2024-04-01 12:56:08 +0100

    cursor, icons, page style, reset button

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # week02-project
+
 🍪 Cookie Clicker
+
+🎯 Please mention the requirements you met and which goals you achieved for this assignment.
+
+For this assignment I:
+
+- designed a wireframe including the main game sections
+- built out some of the wireframe using HTML and CSS
+- added a cookie and styled buttons
+- added event listeneres to the buttons to increment the cookie counter and cookies per second
+- added a CSS animation when the button is pressed
+
+🎯 Were there any requirements or goals that you were not quite able to achieve?
+
+🎯 If so, could you please tell us what was it that you found difficult about these tasks?
+
+What went really well and what could have gone better?
+
+Detailing useful external sources that helped you complete the assignment (e.g Youtube tutorials).
+
+Describing errors or bugs you encountered while completing your assignment.
+
+Requesting feedback about a specific part of your submission.
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Buttons
 const cookieBtn = document.getElementById("cookieBtn");
 const upgradeBtn1 = document.getElementById("upgradeBtn1");
 const upgradeBtn2 = document.getElementById("upgradeBtn2");
@@ -5,11 +6,12 @@ const upgradeBtn3 = document.getElementById("upgradeBtn3");
 const upgradeBtn4 = document.getElementById("upgradeBtn4");
 const upgradeBtn5 = document.getElementById("upgradeBtn5");
 const upgradeBtn6 = document.getElementById("upgradeBtn6");
+const resetBtn = document.getElementById("resetBtn");
+const allCats = document.querySelector(".cat");
 
+//Text
 const counterNumber = document.getElementById("counterNumber");
 const cpsNumber = document.getElementById("cpsNumber");
-
-const catInfo1 = document.getElementById("cat-info-one");
 
 const gameStats = {
   cookies: 0,
@@ -30,7 +32,7 @@ function makeCookie() {
   updateStorage();
 }
 
-//functions for upgrade buttons
+//Upgrade button functions
 function buyUpgrade() {
   let currentCookies = counterNumber.textContent;
   if (currentCookies >= 10) {
@@ -43,9 +45,9 @@ function buyUpgrade() {
 
 function buyUpgrade2() {
   let currentCookies = counterNumber.textContent;
-  if (currentCookies >= 500) {
+  if (currentCookies >= 200) {
     gameStats.cps = gameStats.cps + 5;
-    gameStats.cookies -= 500;
+    gameStats.cookies -= 200;
     updatePage();
     updateStorage();
   }
@@ -101,6 +103,7 @@ function updateStorage() {
   localStorage.setItem("gameStats", JSON.stringify(gameStats));
 }
 
+//Button events
 cookieBtn.addEventListener("click", makeCookie);
 upgradeBtn1.addEventListener("click", buyUpgrade);
 upgradeBtn2.addEventListener("click", buyUpgrade2);
@@ -119,33 +122,39 @@ setInterval(function () {
 setInterval(function () {
   if (counterNumber.textContent >= 10 || cpsNumber.textContent >= 1) {
     upgradeBtn1.disabled = false;
-    upgradeBtn1.style.backgroundImage =
-      "url('https://cdn-icons-png.freepik.com/512/616/616430.png";
-    catInfo1.style.color = "black";
+    upgradeBtn1.style.backgroundImage = "url('./assets/cat icon.png')";
   }
   if (cpsNumber.textContent >= 50) {
     upgradeBtn2.disabled = false;
-    upgradeBtn2.style.backgroundImage =
-      "url('https://cdn-icons-png.freepik.com/512/616/616430.png')";
+    upgradeBtn2.style.backgroundImage = "url('./assets/cat icon.png')";
   }
   if (cpsNumber.textContent >= 100) {
     upgradeBtn3.disabled = false;
-    upgradeBtn3.style.backgroundImage =
-      "url('https://cdn-icons-png.freepik.com/512/616/616430.png')";
+    upgradeBtn3.style.backgroundImage = "url('./assets/cat icon.png')";
   }
   if (cpsNumber.textContent >= 200) {
     upgradeBtn4.disabled = false;
-    upgradeBtn4.style.backgroundImage =
-      "url('https://cdn-icons-png.freepik.com/512/616/616430.png')";
+    upgradeBtn4.style.backgroundImage = "url('./assets/cat icon.png')";
   }
   if (cpsNumber.textContent >= 1000) {
     upgradeBtn5.disabled = false;
-    upgradeBtn5.style.backgroundImage =
-      "url('https://cdn-icons-png.freepik.com/512/616/616430.png')";
+    upgradeBtn5.style.backgroundImage = "url('./assets/cat icon.png')";
   }
   if (cpsNumber.textContent >= 10000) {
     upgradeBtn6.disabled = false;
-    upgradeBtn6.style.backgroundImage =
-      "url('https://cdn-icons-png.freepik.com/512/616/616430.png')";
+    upgradeBtn6.style.backgroundImage = "url('./assets/cat icon.png')";
   }
 }, 0);
+
+//Reset
+
+function resetGame() {
+  gameStats.cookies = 0;
+  gameStats.cps = 0;
+  updatePage();
+  allCats.disabled = true;
+  allCats.style.backgroundImage =
+    "url('https://img.icons8.com/?size=256&id=121428&format=png')";
+}
+
+resetBtn.addEventListener("click", resetGame);
